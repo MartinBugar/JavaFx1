4c8e95d4eb5eb27bca8c2a0ac3847b1bfd5ffa40
prometheus9911 <martin1.bugar@gmail.com>
2021-01-30 17:53:03 +0100

    All FIXED

--- src/sample/DatabaseConnection.java
+++ src/sample/DatabaseConnection.java
@@ -12,13 +12,11 @@ public class DatabaseConnection {
         String databaseName = "demo_db";
         String databaseUser = "root";
         String databasePassword = "Heslo123";
-        String timeZone = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-       // String url = "jdbc:mysql://localhost3360/" + databaseName + timeZone;
-        String url2 = "jdbc:mysql://localhost:3306/demo_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://localhost:3306/demo_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            databaseLink = DriverManager.getConnection(url2, databaseUser,databasePassword);
+            databaseLink = DriverManager.getConnection(url, databaseUser,databasePassword);
         } catch (Exception ex){
             ex.printStackTrace();
             ex.getCause();
