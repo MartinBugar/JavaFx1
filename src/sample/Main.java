11a2a4fc1bf8481783a921163cac3ad6cb94615c
prometheus9911 <martin1.bugar@gmail.com>
2021-01-31 09:33:25 +0100

    mousepresses and drag

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,31 +2,45 @@ package sample;
 
 import javafx.application.Application;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class Main extends Application {
 
-
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Login/login.fxml"));
-
-        primaryStage.initStyle(StageStyle.UNDECORATED);
-        primaryStage.setScene(new Scene(root, 600, 378));
-        primaryStage.show();
-
-    }
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(final Stage primaryStage) throws Exception{
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        BorderPane root = FXMLLoader.load(getClass().getResource("Login/login.fxml"));
+
+
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
+        Scene scene = new Scene(root, 600, 378);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
