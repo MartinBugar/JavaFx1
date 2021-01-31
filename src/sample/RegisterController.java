07574d41e53e8e40b6f83241596cbeca5442d803
prometheus9911 <martin1.bugar@gmail.com>
2021-01-31 16:05:24 +0100

    Registration scnebuilder

--- src/sample/RegisterController.java
+++ src/sample/RegisterController.java
@@ -1,15 +1,21 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.File;
 import java.net.URL;
@@ -19,6 +25,9 @@ import java.util.ResourceBundle;
 
 public class RegisterController implements Initializable {
 
+    private double xOffset;
+    private double yOffset;
+
     @FXML
     private ImageView logoImageView;
 
@@ -26,8 +35,39 @@ public class RegisterController implements Initializable {
     private Button closeButton;
 
     public void closeButtonOnAction (ActionEvent actionEvent){
+
         Stage stage = (Stage) closeButton.getScene().getWindow();
         stage.close();
+
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Stage loginStage = new Stage();
+            loginStage.initStyle(StageStyle.UNDECORATED);
+
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+            root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    loginStage.setX(event.getScreenX() - xOffset);
+                    loginStage.setY(event.getScreenY() - yOffset);
+                }
+            });
+
+            Scene scene = new Scene(root, 600, 378);
+            loginStage.setScene(scene);
+            loginStage.show();
+
+        } catch (Exception ex){
+            ex.printStackTrace();
+            ex.getCause();
+        }
     }
 
     @Override
@@ -36,6 +76,5 @@ public class RegisterController implements Initializable {
         Image logoImage = new Image(logoFile.toURI().toString());
         logoImageView.setImage(logoImage);
     }
-
 
 }
